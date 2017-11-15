5a35496fd4861991d70489f3a517a928ade44b7c
Denis Nadeau <dnadeau4@users.noreply.github.com>
2017-11-15 13:42:45 -0800

    change CDTime Exception to ValueException (#11)

--- tests/test_cdtime_components.py
+++ tests/test_cdtime_components.py
@@ -2,6 +2,13 @@ import unittest
 import cdtime
 
 class CDTIMETest(unittest.TestCase):
+
+    def testWrongUnits(self):
+        try:
+            b = cdtime.reltime(4, "days since YYYY-XX")
+        except Exception as inst:
+            self.assertEqual(inst.args[0], "Invalid relative time units")
+
     def testCdtimeComponent(self):
         tc = cdtime.comptime(2017)
         value = 4
@@ -13,4 +20,7 @@ class CDTIMETest(unittest.TestCase):
         tc.second = value
         value = 4.
         tc.second = value
+
+if __name__ == '__main__':
+    unittest.main()
 
