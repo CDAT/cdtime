fdd66609868cad5dba9a3192bb5f1991077099d3
Charles Doutriaux <doutriaux1@llnl.gov>
2018-02-27 14:00:39 -0800

    np name in pkg version

--- ci-support/conda_upload.sh
+++ ci-support/conda_upload.sh
@@ -27,19 +27,18 @@ source activate root
 conda install -q anaconda-client conda-build
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=${HOME}/conda-bld
-export VERSION=$(date +%Y.%m.%d)
 echo "Cloning recipes"
 git clone git://github.com/UV-CDAT/conda-recipes
 cd conda-recipes
 # uvcdat creates issues for build -c uvcdat confises package and channel
 rm -rf uvcdat
-python ./prep_for_build.py -v $(date +%Y.%m.%d)
+python ./prep_for_build.py
 #
 echo "Building and uploading now"
 #
 # use variant configuration in conda_build_config.yaml
 # Bug in conda-build 3.3.0 can be avoided with 3.2.2 on OSX
 #
-conda install conda-build==3.2.2
+conda install conda-build
 conda build -c conda-forge  ${PKG_NAME}  
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-$(date +%Y.%m.%d)-*_0.tar.bz2 --force
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME*_0.tar.bz2 --force
