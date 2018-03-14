7703b9a4753c9687d91a5bd58bde1d871f952301
Charles Doutriaux <doutriaux1@llnl.gov>
2018-03-14 14:57:14 -0700

    pushing to cdat channel

--- ci-support/circleci_mac_dep.sh
+++ ci-support/circleci_mac_dep.sh
@@ -2,8 +2,8 @@
 ls
 pwd
 export PATH=${HOME}/miniconda/bin:${PATH}
-conda create -n py3 -c uvcdat/label/nightly -c conda-forge -c uvcdat cdat_info libcdms libdrs_f numpy  flake8 nose "python>3"
-conda create -n py2 -c uvcdat/label/nightly -c conda-forge -c uvcdat cdat_info libcdms libdrs_f numpy  flake8 nose
+conda create -n py3 -c cdat/label/nightly -c uvcdat/label/nightly -c conda-forge -c uvcdat cdat_info libcdms libdrs_f numpy  flake8 nose "python>3"
+conda create -n py2 -c cdat/label/nightly -c uvcdat/label/nightly -c conda-forge -c uvcdat cdat_info libcdms libdrs_f numpy  flake8 nose
 export UVCDAT_ANONYMOUS_LOG=False
 source activate py3
 python setup.py install
