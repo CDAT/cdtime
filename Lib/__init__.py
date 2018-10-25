477280a6e25091003e4e60729e34ea14e8bab61f
Nadeau <nadeau1@llnl.gov>
2018-10-24 21:11:25 -0700

    fix nostest issue with cdtime.cdtime package

--- Lib/__init__.py
+++ Lib/__init__.py
@@ -1,9 +1,9 @@
 
 
-__all__ = [['__doc__', '__getstate__', '__module__', '__reduce__', '__setstate__', '_mrreconstruct', 'abstime',
-            'c2r', 'compare', 'componenttime', 'comptime', 'r2c', 'r2r', 'relativetime', 'reltime', 's2c', 's2r']]
+__all__ = ['__doc__', 'abstime', 'c2r', 'compare', 'componenttime', 'comptime', 
+        'r2c', 'r2r', 'relativetime', 'reltime', 's2c', 's2r']
 
 import numpy
 
-from .cdtime import *
+from .compreltime import *
 dump = numpy.ndarray.dump
