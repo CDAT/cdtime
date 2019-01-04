14b6073d22e3116b5a6bb60a2fa129c2b86dce98
Denis Nadeau <nadeau1llnl.gov>
2019-01-04 15:02:54 -0800

    expose all periods and calendar in __all__

--- Lib/__init__.py
+++ Lib/__init__.py
@@ -1,7 +1,11 @@
 
 
-__all__ = ['__doc__', 'abstime', 'c2r', 'compare', 'componenttime', 'comptime', 
-        'r2c', 'r2r', 'relativetime', 'reltime', 's2c', 's2r']
+__all__ = ['__doc__', 'abstime', 'c2r', 'compare', 'componenttime', 'comptime',
+        'r2c', 'r2r', 'relativetime', 'reltime', 's2c', 's2r', 'Calendar360',
+        'ClimCalendar', 'ClimLeapCalendar', 'Day', 'Days', 'DefaultCalendar',
+        'GregorianCalendar', 'Hour', 'Hours', 'JulianCalendar', 'Minute', 'Minutes',
+        'MixedCalendar', 'Month', 'Months', 'NoLeapCalendar', 'Season', 'Seasons',
+        'Second', 'Seconds', 'StandardCalendar', 'Week', 'Weeks', 'Year', 'Years']
 
 import numpy
 
