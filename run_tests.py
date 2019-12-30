fd5b406260b4adc5a376d8657ee26740d8e0d76c
muryanto1 <muryanto1@llnl.gov>
2019-12-30 12:07:28 -0800

    added 'if __name__ == __main__' in run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -5,8 +5,10 @@ import cdat_info
 test_suite_name = 'cdtime'
 
 workdir = os.getcwd()
-runner = cdat_info.TestRunnerBase(test_suite_name)
-ret_code = runner.run(workdir)
+ret_code = SUCCESS
+if __name__ == '__main__':
+    runner = cdat_info.TestRunnerBase(test_suite_name)
+    ret_code = runner.run(workdir)
 
 sys.exit(ret_code)
 
