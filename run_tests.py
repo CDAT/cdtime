eeed7fc094969fe03ac7b54106052ce39e21b9b7
muryanto1 <muryanto1@llnl.gov>
2020-01-02 10:45:32 -0800

    restore run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -5,10 +5,8 @@ import cdat_info
 test_suite_name = 'cdtime'
 
 workdir = os.getcwd()
-ret_code = 0
-if __name__ == '__main__':
-    runner = cdat_info.TestRunnerBase(test_suite_name)
-    ret_code = runner.run(workdir)
+runner = cdat_info.TestRunnerBase(test_suite_name)
+ret_code = runner.run(workdir)
 
 sys.exit(ret_code)
 
